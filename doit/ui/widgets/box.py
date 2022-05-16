34430d795042e01fcb5b4da6166b34c8aedc5010
kraanzu <kraanzu@gmail.com>
2022-05-16 08:57:33 +0530

    feat: Add header `Box` Widget

---
+++ doit/ui/widgets/box.py
@@ -0,0 +1,27 @@
+from rich.align import Align
+from rich.console import RenderableType
+from rich.panel import Panel
+from rich.style import StyleType
+from textual.widget import Widget
+
+
+class Box(Widget):
+    def __init__(self, name: str | None = None, color: StyleType = "green") -> None:
+        super().__init__(name)
+        self.color = color
+        self.highlighted = False
+
+    def render(self) -> RenderableType:
+        return Panel(
+            Align.center(self.name, vertical="middle"),
+            border_style=("bold " if self.highlighted else "dim ") + str(self.color),
+        )
+
+    def highlight(self):
+        self.highlighted = True
+        self.refresh()
+
+    def lowlight(self):
+        self.highlighted = False
+        self.refresh()
+

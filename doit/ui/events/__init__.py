9e519748ed15597489dbd7248d271b622f964ad9
kraanzu <kraanzu@gmail.com>
2022-05-30 09:48:05 +0530

    fix: not handled event

--- doit/ui/events/__init__.py
+++ doit/ui/events/__init__.py
@@ -1,8 +1,8 @@
-from .events import Keystroke, MenuOptionChange, ChangeStatus, PostMessage
+from .events import Keystroke, MenuOptionChange, ChangeStatus, Statusmessage
 
 __all__ = [
     "Keystroke",
     "MenuOptionChange",
     "ChangeStatus",
-    "PostMessage",
+    "Statusmessage",
 ]

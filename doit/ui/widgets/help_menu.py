6f15db579bbb3cbdb38732c0d1499c8fa281990e
kraanzu <kraanzu@gmail.com>
2022-06-15 08:56:13 +0530

    feat: Add `Help Menu`

--- doit/ui/widgets/help_menu.py
+++ doit/ui/widgets/help_menu.py
@@ -19,23 +19,31 @@ def colored(text: str, color: StyleType) -> str:
     return f"[{color}]{text}[/{color}]"
 
 
-def generate_kb_table(kb: dict[str, str], topic: str) -> RenderableType:
+def generate_kb_table(
+    kb: dict[str, str], topic: str, notes: list[str] = []
+) -> RenderableType:
     table = Table.grid(expand=True, padding=(0, -1))
     table.add_column("mode", width=12)
     table.add_column("cmd", width=10)
     table.add_column("colon", width=2)
     table.add_column("help")
 
-    table.add_row(Text.from_markup(f" [r blue] {topic} [/r blue]"), "", "", "")
+    table.add_row(Text.from_markup(f" [r green] {topic} [/r green]"), "", "", "")
     for cmd, help in kb.items():
         table.add_row(
             "",
             (Text.from_markup(colored(cmd, "blue"))),
             "",
-            (Text.from_markup(colored(" " + help, "magenta")) + NL),
+            (Text.from_markup(colored("  " + help, "magenta")) + NL),
         )
 
-    return Align.center(Group(table, NL + seperator + NL))
+    if notes:
+        notes = [f"{colored('', 'd yellow')} {i}" for i in notes]
+        notes = [colored(" Note:", "d white")] + notes
+
+    return Align.center(
+        Group(table, *[Text.from_markup(i) for i in notes], NL + seperator + NL)
+    )
 
 
 seperator = f"{colored('─' * 60, 'bold dim black')}"
@@ -46,29 +54,39 @@ seperator = f"{colored('─' * 60, 'bold dim black')}"
 # KEYBINDINGS
 # --------------------------------------------
 NORMAL_KB = {
-    "j/down": "move down",
-    "k/up": "move up",
-    "i": "edit data",
-    "g": "move to top",
-    "G": "move to bottom",
-    "z": "toggle-expand highlighted node",
-    "Z": "toggle-expand parent node",
-    "x": "remove highlighted node",
-    "a": "add sibling",
-    "A": "add child",
+    "j/down": "Move down in list",
+    "k/up": "Move up in list",
+    "i": "Edit todo/topic",
+    "d": "Edit date ⃰ ",
+    "g": "Move to top of list",
+    "G": "Move to bottom of list",
+    "z": "Toggle-expand highlighted item",
+    "Z": "Toggle-expand parent item",
+    "x": "Remove highlighted node",
+    "a": "Add sibling todo/topic",
+    "A": "Add child todo/topic",
+    "enter": "Select topic ⃰ ",
+    "escape": "Move focus to Menu ⃰ ⃰ ",
 }
 
+NORMAL_NB = [
+    f"{colored('*  - In Menu Only', 'grey50')}",
+    f"{colored('** - In Todo List Only', 'grey50')}",
+]
+
 INSERT_KB = {
-    "escape": "Go back to normal mode",
-    "any": "Enter the key in the focused area",
+    "escape": "Go back to NORMAL mode",
+    "any": "Push key in the selected item",
 }
 
 DATE_KB = {
     "escape": "Go back to normal mode",
-    "any": "Enter the key in the focused area"
-    + "\n"
-    + "Only digits and hyphen allowed",
+    "any": "Enter the key in the focused area",
 }
+
+DATE_NB = [
+    f"{colored('Only digits and hyphen allowed format:', 'grey50')} {(colored('dd-mm-yyyy', 'yellow'))}"
+]
 
 SEARCH_KB = {
     "escape": "Navigate in the searched items"
@@ -93,15 +111,16 @@ HEADER = f"""
 {seperator}
 """
 
-BODY = f"""
-{colored(f'Doit is build to be used from the keyboard,{NL} but mouse can also be used to navigate', 'green')}
+BODY = f""" {colored(f'Doit is build to be used from the keyboard,{NL} but mouse can also be used to navigate', 'green')}
 
 Documentation below will wak you through the controls:
 {seperator}
 """
 
-FOOTER = f"{colored('Thanks for using doit :heart:', 'yellow')}"
-AUTHOR = f"{colored('--kraanzu', 'orchid')}"
+THANKS = f"{colored('Thanks for using doit :heart:', 'yellow')}"
+AUTHOR = f"{colored('--kraanzu', 'orchid')}{NL * 2}{seperator}{NL}"
+
+OUTRO = f"Press {colored('escape', 'green')} or {colored('ctrl+p', 'green')} to exit help menu"
 
 # ---------------- X -------------------------
 
@@ -113,8 +132,9 @@ class HelpMenu(Widget):
 
     header = Text.from_markup(HEADER, justify="center")
     body = Text.from_markup(BODY, justify="center")
-    footer = Text.from_markup(FOOTER, justify="center")
+    thanks = Text.from_markup(THANKS, justify="center")
     author = Text.from_markup(AUTHOR, justify="center")
+    outro = Text.from_markup(OUTRO, justify="center")
 
     def render(self) -> RenderableType:
         tree = Tree("")
@@ -123,12 +143,13 @@ class HelpMenu(Widget):
 
         tree.add(self.header)
         tree.add(self.body)
-        tree.add(generate_kb_table(NORMAL_KB, "NORMAL"))
+        tree.add(generate_kb_table(NORMAL_KB, "NORMAL", NORMAL_NB))
         tree.add(generate_kb_table(INSERT_KB, "INSERT"))
-        tree.add(generate_kb_table(DATE_KB, "DATE"))
+        tree.add(generate_kb_table(DATE_KB, "DATE", DATE_NB))
         tree.add(generate_kb_table(SEARCH_KB, "SEARCH"))
         tree.add(generate_kb_table(SORT_KB, "SORT"))
-        tree.add(self.footer)
+        tree.add(self.thanks)
         tree.add(self.author)
+        tree.add(self.outro)
 
         return Panel(tree, box=MINIMAL)

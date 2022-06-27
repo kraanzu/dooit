58d348bc8aa3338788adfd9cb6fb4b8f8c083af3
kraanzu <kraanzu@gmail.com>
2022-06-27 07:45:14 +0530

    feat: Add `version` and `help` flags (closes #43)

--- dooit/__init__.py
+++ dooit/__init__.py
@@ -1,5 +1,15 @@
 from .ui.tui import Doit
+import pkg_resources
+import argparse
 
 
 def main():
-    Doit.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", help="Show version", action="store_true")
+    args = parser.parse_args()
+
+    if args.version:
+        ver = pkg_resources.get_distribution("dooit").version
+        print(f"dooit - {ver}")
+    else:
+        Doit.run()

8a7faae77a7a84d0809d07d1d5eec6e545a726a8
kraanzu <kraanzu@gmail.com>
2022-08-19 17:06:22 +0530

    feat: Dooit's api functions

---
+++ dooit/api/todo_manager.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+from typing import Literal, Optional, Union
+from ..utils import Urgency, Todo, Topic, WorkSpace
+
+DateType = Union[str, datetime]
+SortMethodType = Literal["name", "status", "date", "urgency"]
+FilterMethod = Literal["about", "date", "urgency"]
+
+
+class Manager:
+    """
+    A Manager backend for dooit
+    """
+
+    def __init__(self) -> None:
+        self.topics: dict[str, Topic] = dict()
+        self.workspaces: dict[str, WorkSpace] = dict()
+
+    def add_workspace(self, workspace: str):
+        self.workspaces[workspace] = WorkSpace(workspace)
+
+    def rename_workspace(self, workspace: str, name: str):
+        self.workspaces[name] = self.workspaces[workspace]
+        del self.workspaces[workspace]
+
+    def remove_workspace(self, workspace: str):
+        del self.workspaces[workspace]
+
+    def add_topic(self, workspace: str, topic: str):
+        self.workspaces[workspace].add_topic(topic)
+
+    def rename_topic(self, workspace: str, topic: str, name: str):
+        self.workspaces[workspace].rename_topic(topic, name)
+
+    def remove_topic(self, workspace: str, topic):
+        self.workspaces[workspace].remove_topic(topic)
+
+    def add_todo(
+        self,
+        workspace: str,
+        topic: str = "common",
+        about: Optional[str] = None,
+        due: Optional[DateType] = None,
+        urgency: Optional[Urgency] = None,
+    ):
+        ...
+
+    def edit_todo(
+        self,
+        workspace: str,
+        topic: str = "common",
+        about: Optional[str] = None,
+        due: Optional[DateType] = None,
+        urgency: Optional[Urgency] = None,
+    ):
+        ...
+
+    def sort_todos(
+        self,
+        workspace: str,
+        *,
+        topic: str = "common",
+        method: SortMethodType,
+    ):
+        ...
+
+    def filter_todos(
+        self, workspace: str, *, topic: str = "common", method: FilterMethod
+    ):
+        ...
+
+
+MANAGER = Manager()

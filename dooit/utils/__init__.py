e7de61c7e1b0939936a972728d8acec7d3d90066
kraanzu <kraanzu@gmail.com>
2022-08-19 17:06:06 +0530

    chore: Add classes to `__init__`

--- dooit/utils/__init__.py
+++ dooit/utils/__init__.py
@@ -1,8 +1,16 @@
 from .parser import Parser
 from .config import Config, conf
+from .urgency import Urgency
+from .topic import Topic
+from .todo import Todo
+from .workspace import WorkSpace
 
 __all__ = [
     "Parser",
     "Config",
     "conf",
+    "Urgency",
+    "WorkSpace",
+    "Todo",
+    "Topic",
 ]

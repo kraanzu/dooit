bb055b1b0f91d2d8572db429c2e445ff7bec99d8
Murli Tawari <kraanzu@gmail.com>
2022-10-16 18:54:02 +0530

    feat: Add default config (beta)

---
+++ dooit/utils/default_config.py
@@ -0,0 +1,55 @@
+# NOTE: See rich style documentation for details
+
+#################################
+#            NAVBAR             #
+#################################
+# Vars:
+# name: name of the workspace/topic
+# icon: icon to show for workspace
+
+navbar = {
+    "icon": "",
+    "fmt": {
+        "highlight": "",
+        "dim": "",
+        "edit": "",
+    },
+}
+
+#################################
+#            TODOS              #
+#################################
+# Vars:
+# name: name of the workspace/topic
+# icon: icon to show for workspace and topic
+todos = {
+    "icon": {
+        "status": {
+            "done": "",
+            "pending": "",
+            "overdue": "",
+        },
+        "urgency": {
+            1: "🅓",
+            2: "🅒",
+            3: "🅑",
+            4: "🅐",
+        },
+    },
+    "fmt": {
+        "highlight": "",
+        "dim": "",
+        "edit": "",
+    },
+}
+
+#################################
+#          STATUS BAR           #
+#################################
+# TOP
+
+# BOTTOM
+
+#################################
+#          KEYBINDING           #
+#################################

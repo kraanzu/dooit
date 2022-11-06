a598d255acf3c2fc89fccc93453902676d81e347
Murli Tawari <kraanzu@gmail.com>
2022-11-06 08:08:40 +0530

    refactor: Better main func

--- dooit/__init__.py
+++ dooit/__init__.py
@@ -1,17 +1,15 @@
+import argparse
+import pkg_resources
 from .ui.tui import Dooit
 
-def main():
-    Dooit.run()
 
-#
-#
-# def main():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("-v", "--version", help="Show version", action="store_true")
-#     args = parser.parse_args()
-#
-#     if args.version:
-#         ver = pkg_resources.get_distribution("dooit").version
-#         print(f"dooit - {ver}")
-#     else:
-#         Dooit.run()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", help="Show version", action="store_true")
+    args = parser.parse_args()
+
+    if args.version:
+        ver = pkg_resources.get_distribution("dooit").version
+        print(f"dooit - {ver}")
+    else:
+        Dooit.run()

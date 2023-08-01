f1d66a95a70cdf0ffb64aeef14bb14de9044fdc2
kraanzu <kraanzu@gmail.com>
2023-08-01 08:15:28 +0530

    change code to use `parsedatetime` (closes #136)

--- dooit/utils/date_parser.py
+++ dooit/utils/date_parser.py
@@ -1,11 +1,14 @@
-import dateutil.parser
+from datetime import datetime
+from typing import Optional
+import parsedatetime
 from os import environ
 
 DATE_ORDER = environ.get("DOOIT_DATE_ORDER", "DMY")
+cal = parsedatetime.Calendar()
 
 
-def parse(value: str):
+def parse(value: str) -> Optional[datetime]:
     if value == "none":
         return None
 
-    return dateutil.parser.parse(value)
+    return cal.parseDT(value)[0]

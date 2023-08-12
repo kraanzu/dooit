c660c9565665e594e36fdd95059db553c23a716b
kraanzu <kraanzu@gmail.com>
2023-08-12 18:07:39 +0530

    fix: remove generic `T`

--- dooit/api/__init__.py
+++ dooit/api/__init__.py
@@ -1,6 +1,6 @@
-from .model import Model, T
+from .model import Model
 from .manager import Manager, manager
 from .todo import Todo
 from .workspace import Workspace
 
-__all__ = ["Model", "Manager", "Todo", "Workspace", "T", "manager"]
+__all__ = ["Model", "Manager", "Todo", "Workspace", "manager"]

5e8452783c7eddb767030d8dad6212e9316bef00
kraanzu <kraanzu@gmail.com>
2023-08-17 09:30:36 +0530

    fix: recursion error on toggle complete

--- dooit/ui/widgets/node.py
+++ dooit/ui/widgets/node.py
@@ -136,8 +136,8 @@ class Node(Widget):
             return self.model.parent.uuid
 
     async def refresh_value(self, input_type: Optional[Type] = None) -> None:
-        for i in self.query(self.__class__):
-            await self.refresh_value()
+        # for i in self.query(self.__class__):
+        #     await self.refresh_value()
 
         input_type = input_type or SimpleInput
         for i in self.query(input_type):

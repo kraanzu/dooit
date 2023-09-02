62ea131fe1db610396ad09858cdefaadc7e366bc
kraanzu <kraanzu@gmail.com>
2023-09-02 09:47:17 +0530

    chore: modify date_parser to use `dateutil`

--- dooit/utils/date_parser.py
+++ dooit/utils/date_parser.py
@@ -1,23 +1,13 @@
 from datetime import datetime
 from typing import Optional, Tuple
-import parsedatetime
+from dateutil import parser
 from os import environ
 
 DATE_ORDER = environ.get("DOOIT_DATE_ORDER", "DMY")
-cal = parsedatetime.Calendar()
 
 
 def parse(value: str) -> Tuple[Optional[datetime], bool]:
-    if value == "none":
-        return None, True
-
-    is_time_included = any(i in value.lower() for i in [":", "@", "at", "am", "pm"])
-
-    parsed, ok = cal.parseDT(value)
-    if not ok:
+    try:
+        return parser.parse(value), True
+    except:
         return None, False
-
-    if not is_time_included:
-        parsed = datetime(parsed.year, parsed.month, parsed.day)
-
-    return parsed, True

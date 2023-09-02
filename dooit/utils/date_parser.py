f166a93e4b6918ad604ada2705f5de1786ca8f97
kraanzu <kraanzu@gmail.com>
2023-09-02 17:21:57 +0530

    fix: circular import error

--- dooit/utils/date_parser.py
+++ dooit/utils/date_parser.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from typing import Optional, Tuple
 from dateutil import parser
-from dooit.utils.conf_reader import config_man
-
-DAY_FIRST = config_man.get("USE_DAY_FIRST")
 
 
 def parse(value: str) -> Tuple[Optional[datetime], bool]:
+    from dooit.utils.conf_reader import config_man
+
+    DAY_FIRST = config_man.get("USE_DAY_FIRST")
     try:
         return parser.parse(value, dayfirst=DAY_FIRST), True
     except:

4e42842f5b7aec543e3449c29db52d767039edb5
Murli Tawari <kraanzu@gmail.com>
2024-02-18 16:51:37 +0530

    Merge pull request #172 from frizd/use-msgpack
    feat: Use msgpack to serialize data

--- dooit/utils/parser.py
+++ dooit/utils/parser.py
@@ -1,12 +1,14 @@
 import appdirs
-import yaml
+import msgpack
 import os
+import yaml
 from typing import Dict
 from pathlib import Path
 from os import makedirs
 
 XDG_CONFIG = Path(appdirs.user_config_dir("dooit"))
 XDG_DATA = Path(appdirs.user_data_dir("dooit"))
+TODO_DATA = XDG_DATA / "todo.dat"
 
 
 class Parser:
@@ -16,7 +18,7 @@ class Parser:
 
     @property
     def last_modified(self) -> float:
-        return os.stat(self.todo_yaml).st_mtime
+        return os.stat(TODO_DATA).st_mtime
 
     def __init__(self) -> None:
         self.check_files()
@@ -26,16 +28,16 @@ class Parser:
         Save the todos to data file
         """
 
-        with open(self.todo_yaml, "w") as stream:
-            yaml.safe_dump(data, stream, sort_keys=False)
+        with open(TODO_DATA, "wb") as stream:
+            stream.write(msgpack.packb(data, use_bin_type=True))
 
     def load(self) -> Dict:
         """
         Retrieves the todos from data file
         """
 
-        with open(self.todo_yaml, "r") as stream:
-            data = yaml.safe_load(stream)
+        with open(TODO_DATA, "rb") as stream:
+            data = msgpack.unpackb(stream.read(), raw=False)
 
         return data
 
@@ -48,13 +50,20 @@ class Parser:
         makedirs(XDG_CONFIG, exist_ok=True)
         makedirs(XDG_DATA, exist_ok=True)
 
-        self.todo_yaml = XDG_DATA / "todo.yaml"
         self.config_file = XDG_CONFIG / "config.py"
+        self.todo_data_yaml = XDG_DATA / "todo.yaml"
 
-        if not Path.is_file(self.todo_yaml):
-            with open(self.todo_yaml, "w") as f:
-                yaml.safe_dump(dict(), f)
+        if Path.is_file(self.todo_data_yaml):
+            self.migrate_to_msgpack()
+        elif not Path.is_file(TODO_DATA):
+            self.save(dict())
 
         if not Path.is_file(self.config_file):
-            with open(self.config_file, "w") as f:
+            with open(self.config_file, "w") as _:
                 pass
+
+    def migrate_to_msgpack(self):
+        with open(self.todo_data_yaml, "r") as stream:
+            self.save(yaml.safe_load(stream))
+
+        os.remove(self.todo_data_yaml)

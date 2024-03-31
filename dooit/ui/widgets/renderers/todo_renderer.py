2b05182ec7d84ff670de5603d1e9f380e0d8cfa2
kraanzu <kraanzu@gmail.com>
2024-03-31 21:17:47 +0530

    feat: add filter checks for renderers

--- dooit/ui/widgets/renderers/todo_renderer.py
+++ dooit/ui/widgets/renderers/todo_renderer.py
@@ -5,6 +5,8 @@ from .base_renderer import BaseRenderer, Todo
 class TodoRender(BaseRenderer):
     @property
     def model(self) -> Todo:
+        if not isinstance(self._model, Todo):
+            raise ValueError(f"Expected Todo, got {type(self._model)}")
         return self._model
 
     def make_renderable(self) -> RenderableType:
--- dooit/ui/widgets/renderers/workspace_renderer.py
+++ dooit/ui/widgets/renderers/workspace_renderer.py
@@ -5,6 +5,8 @@ from .base_renderer import BaseRenderer, Workspace
 class WorkspaceRender(BaseRenderer):
     @property
     def model(self) -> Workspace:
+        if not isinstance(self._model, Workspace):
+            raise ValueError(f"Expected Workspace, got {type(self._model)}")
         return self._model
 
     def make_renderable(self) -> RenderableType:

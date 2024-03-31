0acecff2190ee95fef7bf0dd84bd00f612916483
kraanzu <kraanzu@gmail.com>
2024-03-31 21:16:44 +0530

    refactor: add checks for types

--- dooit/ui/widgets/trees/todos_tree.py
+++ dooit/ui/widgets/trees/todos_tree.py
@@ -5,7 +5,17 @@ from ..renderers.todo_renderer import TodoRender
 class TodosTree(ModelTree):
     @property
     def model(self) -> Todo:
+        if not isinstance(self._model, Todo):
+            raise ValueError(f"Expected Todo, got {type(self._model)}")
+
         return self._model
+
+    def get_option(self, option_id: str) -> TodoRender:
+        option = super().get_option(option_id)
+        if not isinstance(option, TodoRender):
+            raise ValueError(f"Expected TodoRender, got {type(option)}")
+
+        return option
 
     def force_refresh(self) -> None:
         self.clear_options()
--- dooit/ui/widgets/trees/workspaces_tree.py
+++ dooit/ui/widgets/trees/workspaces_tree.py
@@ -8,7 +8,17 @@ from ..renderers.workspace_renderer import WorkspaceRender
 class WorkspacesTree(ModelTree):
     @property
     def model(self) -> Workspace:
+        if not isinstance(self._model, Workspace):
+            raise ValueError(f"Expected Workspace, got {type(self._model)}")
+
         return self._model
+
+    def get_option(self, option_id: str) -> WorkspaceRender:
+        option = super().get_option(option_id)
+        if not isinstance(option, WorkspaceRender):
+            raise ValueError(f"Expected WorkspaceRender, got {type(option)}")
+
+        return option
 
     def _get_children(self, id: str) -> List[Option]:
         obj = [i for i in self.model.workspaces if i.uuid == id][0]

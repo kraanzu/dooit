49fd0a9fdf7a446f6515356490cc1d7a0ad4a2c6
kraanzu <kraanzu@gmail.com>
2024-04-01 16:09:18 +0530

    refactor: better type hints

--- dooit/ui/widgets/renderers/base_renderer.py
+++ dooit/ui/widgets/renderers/base_renderer.py
@@ -1,19 +1,18 @@
 from typing import Union
 from rich.console import RenderableType
 from textual.widgets.option_list import Option
-
-from dooit.api.model import Model
 from dooit.api.todo import Todo
 from dooit.api.workspace import Workspace
 
+ModelType = Union[Todo, Workspace]
 
 class BaseRenderer(Option):
-    def __init__(self, model: Model):
+    def __init__(self, model: ModelType):
         self._model = model
         super().__init__(self.make_renderable(), id=model.uuid)
 
     @property
-    def model(self) -> Union[Todo, Workspace]:
+    def model(self) -> ModelType:
         raise NotImplementedError
 
     def make_renderable(self) -> RenderableType:

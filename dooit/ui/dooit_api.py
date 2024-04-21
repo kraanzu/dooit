4a2fc60f4f6ee704b915d8817f48566cb71f6523
kraanzu <kraanzu@gmail.com>
2024-04-21 17:21:23 +0530

    api: skeleton setup

---
+++ dooit/ui/dooit_api.py
@@ -0,0 +1,13 @@
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from .tui import Dooit
+
+
+class DooitAPI:
+    def __init__(self, app: "Dooit") -> None:
+        self.app = app
+
+    def handle_key(self, key: str) -> None:
+        exit()

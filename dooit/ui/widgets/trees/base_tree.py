361cb4088990a8da12a7409f03d5668a85728b60
kraanzu <kraanzu@gmail.com>
2024-04-28 20:53:36 +0530

    set `focus=True` for trees

--- dooit/ui/widgets/trees/base_tree.py
+++ dooit/ui/widgets/trees/base_tree.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 ModelType = Union[Todo, Workspace]
 
 
-class BaseTree(OptionList, can_focus=False):
+class BaseTree(OptionList, can_focus=True):
     expanded_nodes = defaultdict(bool)
 
     @property

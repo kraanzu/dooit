d4dd0f02b9e6ed29f9fe2f16e34edbcf29e8e78b
kraanzu <kraanzu@gmail.com>
2024-04-28 17:07:36 +0530

    add api events

---
+++ dooit/ui/api/events.py
@@ -0,0 +1,13 @@
+from collections.abc import Callable
+
+
+class EventDecorator:
+    def __init__(self, event_name):
+        self.event_name = event_name
+
+    def __call__(self, func: Callable):
+        setattr(func, "__dooit_event", self.event_name)
+        return func
+
+
+startup = EventDecorator("startup")

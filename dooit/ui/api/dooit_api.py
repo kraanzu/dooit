632134fbf9f74c0f72fdcad4cdc27e7cb8dc6b9a
kraanzu <kraanzu@gmail.com>
2024-04-28 17:10:44 +0530

    more funcs

--- dooit/ui/api/dooit_api.py
+++ dooit/ui/api/dooit_api.py
@@ -1,4 +1,7 @@
+from collections.abc import Callable
 from typing import TYPE_CHECKING
+
+from dooit.ui.api.plug import PluginManager
 
 
 if TYPE_CHECKING:
@@ -8,6 +11,21 @@ if TYPE_CHECKING:
 class DooitAPI:
     def __init__(self, app: "Dooit") -> None:
         self.app = app
+        self.plugin_manager = PluginManager()
+        self.plugin_manager.scan()
+
+    def notify(self, message: str) -> None:
+        self.app.notify(message)
+
+    def disable_arrow_keys(self):
+        pass
+
+    def set_key(self, key: str, callback: Callable) -> None:
+        pass
 
     def handle_key(self, key: str) -> None:
-        exit()
+        pass
+
+    def trigger_event(self, event: str):
+        for obj in self.plugin_manager.events[event]:
+            obj(self)

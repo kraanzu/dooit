d218aacb26217fd3ab994fcc84132087a6f04e52
kraanzu <kraanzu@gmail.com>
2024-07-04 20:48:06 +0530

    make renderers a module

---
+++ dooit/ui/widgets/renderers/__init__.py
@@ -0,0 +1,10 @@
+from .base_renderer import BaseRenderer
+from .todo_renderer import TodoRender
+from .workspace_renderer import WorkspaceRender
+
+
+__all__ = [
+    "BaseRenderer",
+    "TodoRender",
+    "WorkspaceRender",
+]

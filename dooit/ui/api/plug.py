09840f0993342dac341980245317e731f0947553
kraanzu <kraanzu@gmail.com>
2024-07-20 21:19:41 +0530

    load default file

--- dooit/ui/api/plug.py
+++ dooit/ui/api/plug.py
@@ -2,12 +2,13 @@ from collections import defaultdict
 from pathlib import Path
 from typing import Callable
 from appdirs import user_config_dir
-from .loader import load_dir
+from .loader import load_dir, load_file
 
 MAIN_FOLDER = "dooit_v3"
 
 CONFIG_FOLDER = Path(user_config_dir(MAIN_FOLDER))
 PLUGINS_FOLDER = CONFIG_FOLDER / "plugins"
+DEFAULT_CONFIG = Path(__file__).parent.parent.parent / "utils" / "default_config.py"
 
 
 class PluginManager:
@@ -17,6 +18,7 @@ class PluginManager:
     def scan(self):
         load_dir(self, CONFIG_FOLDER)
         load_dir(self, PLUGINS_FOLDER)
+        load_file(self, DEFAULT_CONFIG)
 
     def _register_event(self, event: str, obj: Callable):
         self.events[event].append(obj)

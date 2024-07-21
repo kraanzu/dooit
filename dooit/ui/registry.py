61f8f6007626680bf2dbf42357b2ee2152304ebe
kraanzu <kraanzu@gmail.com>
2024-07-21 21:00:16 +0530

    add methods for workspace layout

--- dooit/ui/registry.py
+++ dooit/ui/registry.py
@@ -1,13 +1,19 @@
+from dooit.ui.api.components import WorkspaceLayout
+
+
 class Registry:
     """
     Class to store global variables and objects
     """
 
     def __init__(self) -> None:
-        self.variables = {}
+        self.workspace_layout = []
 
-    def set_workspace_layout(self, layout):
-        self.variables["workspace_layout"] = layout
+    def set_workspace_layout(self, layout: WorkspaceLayout):
+        self.workspace_layout = layout
+
+    def get_workspace_layout(self) -> WorkspaceLayout:
+        return self.workspace_layout
 
 
 registry = Registry()

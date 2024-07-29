51826b541422c1168508576c38a9afddc7310ed3
kraanzu <kraanzu@gmail.com>
2024-07-29 09:44:17 +0530

    feat: basic bar impl

--- dooit/ui/widgets/bar/bar.py
+++ dooit/ui/widgets/bar/bar.py
@@ -1,2 +1,38 @@
-class Bar:
-    pass
+from collections.abc import Callable
+from typing import List, Optional
+from rich.console import RenderableType
+from rich.text import TextType
+from rich.table import Table
+from textual.widget import Widget
+
+
+class BarWidget:
+    def __init__(self, func: Callable[..., TextType], width: Optional[int]) -> None:
+        self.func = func
+        self.width = width
+        self.value = self.calculate()
+
+    def calculate(self) -> TextType:
+        self.value = self.func()
+        return self.value
+
+    def get_value(self):
+        return self.value
+
+
+class Bar(Widget):
+    def __init__(self, widgets: List[BarWidget]) -> None:
+        self.widgets = widgets
+
+    def render(self) -> RenderableType:
+        table = Table.grid(expand=True)
+
+        for widget in self.widgets:
+            if widget.width is None:
+                table.add_column("", ratio=1)
+            elif width := widget.width:
+                table.add_column("", width=width)
+            else:
+                table.add_column("")
+
+        return table

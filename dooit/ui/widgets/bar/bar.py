c2061df714babac0922c660d179fb89434148655
kraanzu <kraanzu@gmail.com>
2024-07-29 16:37:14 +0530

    refactor: bar widgets placement

--- dooit/ui/widgets/bar/bar.py
+++ dooit/ui/widgets/bar/bar.py
@@ -7,7 +7,9 @@ from textual.widget import Widget
 
 
 class BarWidget:
-    def __init__(self, func: Callable[..., TextType], width: Optional[int]) -> None:
+    def __init__(
+        self, func: Callable[..., TextType], width: Optional[int] = None
+    ) -> None:
         self.func = func
         self.width = width
         self.value = self.calculate()
@@ -30,16 +32,21 @@ class Bar(Widget):
 
     def set_widgets(self, widgets: List[BarWidget]) -> None:
         self.bar_widgets = widgets
+        self.refresh()
 
     def render(self) -> RenderableType:
         table = Table.grid(expand=True)
 
         for widget in self.bar_widgets:
+            value = widget.get_value()
             if widget.width is None:
-                table.add_column("", ratio=1)
+                table.add_column(width=len(value))
             elif width := widget.width:
-                table.add_column("", width=width)
+                table.add_column(width=width)
             else:
-                table.add_column("")
+                table.add_column(ratio=1)
+
+        values = [widget.get_value() for widget in self.bar_widgets]
+        table.add_row(*values)
 
         return table

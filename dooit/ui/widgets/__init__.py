0776fdfa2046a36d72b7b5de141fb19a616bdc07
kraanzu <kraanzu@gmail.com>
2024-07-29 16:34:23 +0530

    add bar widget to pkgs

--- dooit/ui/widgets/__init__.py
+++ dooit/ui/widgets/__init__.py
@@ -1,10 +1,11 @@
 from .sort_options import SortOptions
 from .help_menu import HelpMenu
-from .bar import Bar
+from .bar import Bar, BarWidget
 
 
 __all__ = [
     "SortOptions",
     "HelpMenu",
     "Bar",
+    "BarWidget",
 ]

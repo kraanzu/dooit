a05af5ad60f1c23578d781d2a9b574543b2ec2ae
kraanzu <kraanzu@gmail.com>
2024-07-31 12:46:28 +0530

    feat: add more dooit exceptions

--- dooit/api/exceptions.py
+++ dooit/api/exceptions.py
@@ -2,3 +2,21 @@ class DooitError(Exception):
     """
     Base class for all exceptions raised by the API.
     """
+
+
+class SiblingAdditionError(DooitError):
+    """
+    Raised when user tries to add a sibling to a non-parented node (i.e Manager Object)
+    """
+
+
+class WorkspaceAdditionError(DooitError):
+    """
+    Raised when user tries to add a workspace to a todo object
+    """
+
+
+class TodoAdditionError(DooitError):
+    """
+    Raised when user tries to add a todo to manager class
+    """

ca8d34f58743fddbefce7acf68a2a3ebf28c27fc
kraanzu <kraanzu@gmail.com>
2024-08-02 20:40:01 +0530

    add `engine` var to package

--- dooit/api/__init__.py
+++ dooit/api/__init__.py
@@ -2,5 +2,6 @@ from .model import Model
 from .manager import Manager, manager
 from .todo import Todo
 from .workspace import Workspace
+from ._vars import engine
 
-__all__ = ["Model", "Manager", "Todo", "Workspace", "manager"]
+__all__ = ["Model", "Manager", "Todo", "Workspace", "manager", "engine"]

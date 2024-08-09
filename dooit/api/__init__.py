b5a91a0daecc261e94bda48f86e3b084aa986372
kraanzu <kraanzu@gmail.com>
2024-08-09 19:40:33 +0530

    remove manager from pkg

--- dooit/api/__init__.py
+++ dooit/api/__init__.py
@@ -1,7 +1,6 @@
 from .model import Model
-from .manager import Manager, manager
 from .todo import Todo
 from .workspace import Workspace
 from ._vars import engine
 
-__all__ = ["Model", "Manager", "Todo", "Workspace", "manager", "engine"]
+__all__ = ["Model", "Todo", "Workspace", "engine"]

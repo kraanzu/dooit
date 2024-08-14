cd94c6dfbc0c72fa0809febdfc7c86c96d2642a6
kraanzu <kraanzu@gmail.com>
2024-08-14 15:48:19 +0530

    test: add sibling test

--- tests/test_core/test_workspace.py
+++ tests/test_core/test_workspace.py
@@ -40,13 +40,13 @@ class TestWorkspace(unittest.TestCase):
 
         self.assertEqual(len(result), 5)
 
-    # def test_workspace_siblings_by_creation(self):
-    #     for _ in range(5):
-    #         w = Workspace()
-    #         w.save(self.session)
-    #
-    #     query = select(Workspace)
-    #     workspace = self.session.execute(query).scalars().first()
-    #
-    #     assert workspace is not None
-    #     self.assertEqual(len(workspace.get_siblings(session=self.session)), 5)
+    def test_workspace_siblings_by_creation(self):
+        for _ in range(5):
+            w = Workspace()
+            w.save(self.session)
+
+        query = select(Workspace)
+        workspace = self.session.execute(query).scalars().first()
+
+        assert workspace is not None
+        self.assertEqual(len(workspace.get_siblings(session=self.session)), 5)

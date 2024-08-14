9ea89c4292a084fc21a7ccec574ab76b2b1147b3
kraanzu <kraanzu@gmail.com>
2024-08-14 15:47:28 +0530

    test: add `setUp` and `tearDown`

--- tests/test_core/test_workspace.py
+++ tests/test_core/test_workspace.py
@@ -13,6 +13,17 @@ class TestWorkspace(unittest.TestCase):
         cls.session = Session(bind=cls.engine)
         Base.metadata.create_all(bind=cls.engine)
 
+    def setUp(self):
+        self.engine = create_engine("sqlite:///:memory:")
+        self.session = Session(bind=self.engine)
+        Base.metadata.create_all(bind=self.engine)
+
+    def tearDown(self) -> None:
+        self.session.rollback()
+        self.session.close()
+
+    # --------------------------------------------------------------
+
     def test_workspace_session(self):
         w = Workspace()
         w.save(session=self.session)

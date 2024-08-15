e6992cbb22d81de81db32f20cfea599c410be166
kraanzu <kraanzu@gmail.com>
2024-08-15 21:06:30 +0530

    feat: add better hooks + attach root after `CREATE`

--- dooit/api/hooks.py
+++ dooit/api/hooks.py
@@ -1,20 +1,27 @@
-from typing import Type, Union
-from sqlalchemy.orm import Session
-from sqlalchemy import select, event
-
+from sqlalchemy import Connection, Table, event
 from .workspace import Workspace
 from .todo import Todo
 
 
-@event.listens_for(Session, "before_commit")
-def fix_order_id(session: Session):
+@event.listens_for(Workspace, "before_update")
+@event.listens_for(Workspace, "before_insert")
+def fix_order_id_workspace(mapper, connection, target: Workspace):
 
-    def fix(cls: Union[Type[Workspace], Type[Todo]]):
-        query = select(cls).where(cls.order_index == -1)
-        objs = session.execute(query).scalars().all()
+    if target.is_root:
+        return
 
-        for obj in objs:
-            obj.order_index = len(obj.get_siblings(session))
+    if target.order_index is None or target.order_index == -1:
+        target.order_index = len(target.siblings)
 
-    fix(Workspace)
-    fix(Todo)
+
+@event.listens_for(Todo, "before_insert")
+@event.listens_for(Todo, "before_update")
+def fix_order_id_todo(mapper, connection, target: Todo):
+
+    if target.order_index is None or target.order_index == -1:
+        target.order_index = len(target.siblings)
+
+
+@event.listens_for(Workspace.__table__, "after_create")
+def create_root(target: Table, connection: Connection, **kw):
+    connection.execute(target.insert(), {"is_root": True})

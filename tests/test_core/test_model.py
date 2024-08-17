b81efba3ea4159861b79612c915458a31cecf87c
kraanzu <kraanzu@gmail.com>
2024-08-17 17:58:20 +0530

    tests: add creation_and_deletion

--- tests/test_core/test_model.py
+++ tests/test_core/test_model.py
@@ -5,6 +5,15 @@ from tests.test_core._base import CoreTestBase
 class TestModel(CoreTestBase):
     # Using Workspace as an example because Model is an abstract class
 
+    def test_creation_and_deletion(self):
+
+        w = Workspace()
+        w.save()
+        self.assertEqual(len(Workspace.all()), 1)
+
+        w.drop()
+        self.assertEqual(len(Workspace.all()), 0)
+
     def test_shifts(self):
         for _ in range(5):
             w = Workspace()
@@ -20,14 +29,3 @@ class TestModel(CoreTestBase):
         workspace.shift_down()
         siblings = workspace.siblings
         self.assertEqual(siblings[1].id, workspace.id)
-
-    def test_sibling_addition(self):
-        for _ in range(5):
-            w = Workspace()
-            w.save()
-
-        workspace = Workspace.all()[0]
-        workspace2 = Workspace()
-        workspace2.save()
-
-        self.assertEqual(workspace2.order_index, 5)

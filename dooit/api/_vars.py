10289d129e654fb9b755b3bf35c0e3d243ef81fd
kraanzu <kraanzu@gmail.com>
2024-09-14 11:02:25 +0530

    fix: dont use local .db file

--- dooit/api/_vars.py
+++ dooit/api/_vars.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 from appdirs import user_data_dir
 
+# ROOT_FOLDER = Path(__file__).parent.parent.parent.absolute()  # local db file for testing
 
 ROOT_FOLDER = Path(user_data_dir("dooit"))
-ROOT_FOLDER = Path(__file__).parent.parent.parent.absolute()  # TODO: remove this line
-
 DATABASE_FILE = ROOT_FOLDER / "dooit.db"
 DATABASE_CONN_STRING = f"sqlite:////{DATABASE_FILE}"

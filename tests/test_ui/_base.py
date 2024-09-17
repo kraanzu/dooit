87f3e538cedb791ea1d68f0ad08beab2eab8dd62
kraanzu <kraanzu@gmail.com>
2024-09-17 21:20:07 +0530

    refactor: cleanup + simpler imports

--- tests/test_ui/_base.py
+++ tests/test_ui/_base.py
@@ -1,20 +1,4 @@
 from dooit.ui.tui import Dooit
 
 TEMP_CONN = "sqlite:///:memory:"
-base_app = Dooit(connection_string=TEMP_CONN)
-
-
-# class UITestBase(IsolatedAsyncioTestCase):
-#     pilot: Pilot
-#
-#     async def asyncSetupClass(self):
-#         async with Dooit(connection_string=TEMP_CONN).run_test() as pilot:
-#             self.pilot = pilot
-#
-#     async def asyncSetup(self):
-#         async with Dooit(connection_string=TEMP_CONN).run_test() as pilot:
-#             self.pilot = pilot
-#
-#     def tearDown(self) -> None:
-#         manager.session.rollback()
-#         manager.session.close()
+run_pilot = Dooit(connection_string=TEMP_CONN).run_test
--- tests/test_ui/test_startup.py
+++ tests/test_ui/test_startup.py
@@ -1,9 +1,6 @@
-from textual.pilot import Pilot
-from dooit.ui.tui import Dooit
-from dooit.ui.widgets.trees import WorkspacesTree
-TEMP_CONN = "sqlite:///:memory:"
+from tests.test_ui._base import run_pilot
 
 
 async def test_startup():
-    async with Dooit(connection_string=TEMP_CONN).run_test() as pilot:
+    async with run_pilot() as pilot:
         assert pilot.app.is_running

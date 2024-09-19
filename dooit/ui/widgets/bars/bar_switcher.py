fa6eb9f652b06882ec74accc344fbbcad20fc37a
kraanzu <kraanzu@gmail.com>
2024-09-19 14:24:58 +0530

    add method to switch to search bar

--- dooit/ui/widgets/bars/bar_switcher.py
+++ dooit/ui/widgets/bars/bar_switcher.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from textual.widgets import ContentSwitcher
 from .status_bar import StatusBar
 from .search_bar import SearchBar
@@ -15,7 +16,15 @@ class BarSwitcher(ContentSwitcher):
 
     async def on_mount(self):
         self.add_content(
-            self.status_bar,
+            widget=self.status_bar,
             id="status_bar",
+            set_current=True,
+        )
+
+    def switch_to_search(self, callback: Callable):
+        search_bar = SearchBar(callback)
+        self.add_content(
+            widget=search_bar,
+            id="search_bar",
             set_current=True,
         )

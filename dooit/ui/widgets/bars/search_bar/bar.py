781f4bd516b549537586fc22253911b30b761a3c
kraanzu <kraanzu@gmail.com>
2024-09-19 14:26:39 +0530

    feat: impl handling and rendering

--- dooit/ui/widgets/bars/search_bar/bar.py
+++ dooit/ui/widgets/bars/search_bar/bar.py
@@ -1,5 +1,22 @@
+from rich.console import RenderableType
+from textual import events
+from typing import Callable
 from .._base import BarBase
+from ...inputs._input import Input
 
 
 class SearchBar(BarBase):
-    pass
+    def __init__(self, callback: Callable, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._search = Input(value="/")
+        self._search.is_editing = True
+        self.callback = callback
+
+    async def handle_key(self, event: events.Key) -> bool:
+        self.notify(event.key)
+        self._search.keypress(event.key)
+        self.refresh()
+        return True
+
+    def render(self) -> RenderableType:
+        return self._search.draw()

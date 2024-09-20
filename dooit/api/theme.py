dc45d391c76f1538adee905c35597f0c71837697
kraanzu <kraanzu@gmail.com>
2024-09-20 16:23:57 +0530

    add basic base theme

---
+++ dooit/api/theme.py
@@ -0,0 +1,24 @@
+class DooitThemeBase:
+    # background colors
+    background_1: str  # Darkest
+    background_2: str  # Lighter
+    background_3: str  # Lightest
+
+    # foreground colors
+    foreground_1: str  # Darkest
+    foreground_2: str  # Lighter
+    foreground_3: str  # Lightest
+
+    # other colors
+    red: str
+    orange: str
+    yellow: str
+    green: str
+    blue: str
+    purple: str
+    pink: str
+    gray: str
+
+    # accent colors
+    primary: str
+    secondary: str

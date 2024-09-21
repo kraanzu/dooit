7ddc7fc6f16aca22ae987921253d5f2eb8e5947c
kraanzu <kraanzu@gmail.com>
2024-09-21 07:48:41 +0530

    refactor + cleanup un-necesary

--- dooit/utils/css_manager.py
+++ dooit/utils/css_manager.py
@@ -11,12 +11,12 @@ def generate_random_id():
 
 
 class CssManager:
-    base_css: Path = Path(__file__).parent.parent.parent / "ui" / "styles.tcss"
+    base_css: Path = Path(__file__).parent.parent / "ui" / "styles.tcss"
     stylesheets: Path = dooit_cache_path / "stylesheets"
-
     css_file: Path = dooit_cache_path / "dooit.tcss"
-    theme_css_file: Path = dooit_cache_path / "theme.tcss"
-    base_css_file: Path = dooit_cache_path / "base.tcss"
+
+    def __init__(self, theme: DooitThemeBase = DooitThemeBase()):
+        self.theme = theme
 
     def _create_files(self):
         if not self.stylesheets.exists():
@@ -25,23 +25,12 @@ class CssManager:
                 exist_ok=True,
             )
 
-        if not self.theme_css_file.exists():
-            self.theme_css_file.touch()
-
-        if not self.base_css_file.exists():
-            self.base_css_file.touch()
-
     def refresh_css(self):
         self._create_files()
-
-        css = ""
-
-        # setup theme variables
-        with open(self.theme_css_file, "r") as f:
-            css = css + "\n" + f.read()
+        css = self.theme.to_css()
 
         # setup base variables
-        with open(self.base_css_file, "r") as f:
+        with open(self.base_css, "r") as f:
             css = css + "\n" + f.read()
 
         # inject extra stylesheets
@@ -52,9 +41,7 @@ class CssManager:
         self.write(css)
 
     def set_theme(self, theme: DooitThemeBase):
-        with open(self.theme_css_file, "w") as f:
-            f.write(theme.to_css())
-
+        self.theme = theme
         self.refresh_css()
 
     def inject_css(self, css: str) -> str:

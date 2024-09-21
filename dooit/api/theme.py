e59c4b62df0d334482fe0960c75a2b054af846c0
kraanzu <kraanzu@gmail.com>
2024-09-21 07:49:05 +0530

    set default theme (nord for now)

--- dooit/api/theme.py
+++ dooit/api/theme.py
@@ -1,27 +1,27 @@
 class DooitThemeBase:
     # background colors
-    background_1: str  # Darkest
-    background_2: str  # Lighter
-    background_3: str  # Lightest
+    background_1: str = "#2E3440"  # Darkest
+    background_2: str = "#3B4252"  # Lighter
+    background_3: str = "#434C5E"  # Lightest
 
     # foreground colors
-    foreground_1: str  # Darkest
-    foreground_2: str  # Lighter
-    foreground_3: str  # Lightest
+    foreground_1: str = "#D8DEE9"  # Darkest
+    foreground_2: str = "#E5E9F0"  # Lighter
+    foreground_3: str = "#ECEFF4"  # Lightest
 
     # other colors
-    red: str
-    orange: str
-    yellow: str
-    green: str
-    blue: str
-    purple: str
-    pink: str
-    gray: str
+    red: str = "#BF616A"
+    orange: str = "#D08770"
+    yellow: str = "#EBCB8B"
+    green: str = "#A3BE8C"
+    blue: str = "#5E81AC"
+    purple: str = "#B48EAD"
+    pink: str = "#B48EAD"  # Nord doesn't have a distinct pink, using purple
+    gray: str = "#4C566A"
 
     # accent colors
-    primary: str
-    secondary: str
+    primary: str = "#88C0D0"
+    secondary: str = "#81A1C1"
 
     def to_css(self) -> str:
         css = f"""

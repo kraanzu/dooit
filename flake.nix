fe3e182b73c3db24c2d2054962f3942ac6d0ac42
kraanzu <kraanzu@gmail.com>
2024-09-22 20:14:05 +0530

    add packaging

--- flake.nix
+++ flake.nix
@@ -13,9 +13,40 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
       in {
+        packages.default = pkgs.python312Packages.buildPythonPackage {
+          pname = "dooit";
+          version = "3.0.0";
+
+          src = ./.;
+
+          format = "pyproject";
+
+          nativeBuildInputs = with pkgs; [
+            poetry # For managing pyproject.toml dependencies
+          ];
+
+          pythonRelaxDeps = [
+            "textual"
+            "tzlocal"
+            "platformdirs"
+          ];
+
+          buildInputs = with pkgs.python312Packages; [
+            poetry-core
+            pyperclip
+            textual
+            pyyaml
+            dateutil
+            sqlalchemy
+            platformdirs
+            tzlocal
+          ];
+        };
+
+        # Optional devShell for development
         devShell = pkgs.mkShell {
           buildInputs = with pkgs.python312Packages; [
-            # main
+            poetry-core
             pyperclip
             textual
             pyyaml

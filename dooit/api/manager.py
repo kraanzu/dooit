5b68d47e66e0f2a1be7e3b60c9b5c86652057fc4
kraanzu <kraanzu@gmail.com>
2024-09-23 21:48:45 +0530

    feat: add functions to determine change by another instance

--- dooit/api/manager.py
+++ dooit/api/manager.py
@@ -1,3 +1,4 @@
+import os
 from typing import Optional
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -19,6 +20,23 @@ class Manager:
         self.session.autoflush = False
 
         BaseModel.metadata.create_all(bind=self.engine)
+        self._db_last_modified = self._get_db_last_modified()
+
+    def _get_db_last_modified(self) -> Optional[float]:
+        database = self.engine.url.database
+        assert database is not None
+
+        try:
+            return os.path.getmtime(database)
+        except OSError:
+            return None
+
+    def has_changed(self) -> bool:
+        current_last_modified = self._get_db_last_modified()
+        if current_last_modified and self._db_last_modified != current_last_modified:
+            self._db_last_modified = current_last_modified
+            return True
+        return False
 
     def delete(self, obj):
         self.session.delete(obj)
@@ -30,6 +48,7 @@ class Manager:
 
     def commit(self):
         self.session.commit()
+        self._db_last_modified = self._get_db_last_modified()
 
 
 manager = Manager()

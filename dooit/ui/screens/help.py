5f67b3e0840436cccdeedf7c90ce4e3d03edc5ab
kraanzu <kraanzu@gmail.com>
2024-09-23 18:05:14 +0530

    remove everything from help screen for now

--- dooit/ui/screens/help.py
+++ dooit/ui/screens/help.py
@@ -1,14 +1,4 @@
-from textual.app import ComposeResult, events
-from textual.widgets import Static
-from dooit.ui.widgets.help_menu import HelpMenu
 from .base import BaseScreen
-
-PRINTABLE = (
-    "0123456789"
-    + "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    + "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ "
-)
-
 
 class HelpScreen(BaseScreen):
     """
@@ -18,18 +8,3 @@ class HelpScreen(BaseScreen):
     BINDINGS = [
         ("escape", "app.pop_screen", "Pop screen"),
     ]
-
-    def compose(self) -> ComposeResult:
-        for i in HelpMenu().items():
-            yield Static(i)
-
-    async def on_key(self, event: events.Key):
-        key = self.resolve_key(event)
-        if key in ["j", "down"]:
-            self.scroll_down()
-        elif key in ["k", "up"]:
-            self.scroll_up()
-        elif key in ["home", "g"]:
-            self.scroll_home()
-        elif key in ["end", "G"]:
-            self.scroll_end()

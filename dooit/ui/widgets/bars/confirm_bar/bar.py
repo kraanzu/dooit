cd3cf28ec7c4e24ee5c0606212ed93b490cfd435
kraanzu <kraanzu@gmail.com>
2024-09-23 17:14:52 +0530

    feat: flash message after comfirmation

--- dooit/ui/widgets/bars/confirm_bar/bar.py
+++ dooit/ui/widgets/bars/confirm_bar/bar.py
@@ -1,22 +1,40 @@
 from typing import Callable
-
 from rich.console import RenderableType
 from textual.widget import events
 from .._base import BarBase
 
+DEFFAULT_MSG = r"Are you sure? \[y/N]"
+
 
 class ConfirmBar(BarBase):
-    def __init__(self, callback: Callable, *args, **kwargs):
+    def __init__(
+        self,
+        callback: Callable,
+        message: str = DEFFAULT_MSG,
+        *args,
+        **kwargs,
+    ):
         super().__init__(callback, *args, **kwargs)
+        self.message = message
 
     def perform_action(self, cancel: bool):
         if not cancel:
             self.callback()
 
+    def flash_confirm(self, cancelled: bool):
+        if cancelled:
+            self.message = "The items were deleted!"
+        else:
+            self.message = "The operation was cancelled!"
+
+        self.refresh()
+        self.set_interval(2, self.close)
+
     async def handle_key(self, event: events.Key) -> bool:
         cancel = event.key.lower() != "y"
-        self.dismiss(cancel)
+        self.flash_confirm(cancel)
+        self.dismiss(cancel, close = False)
         return True
 
     def render(self) -> RenderableType:
-        return r"Are you sure? \[y/N]:"
+        return self.message

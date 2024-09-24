76b46248e7df7052660d7dd84d6a3e5f58650c10
kraanzu <kraanzu@gmail.com>
2024-09-24 20:35:08 +0530

    clean: remove unused imports

--- dooit/ui/widgets/__init__.py
+++ dooit/ui/widgets/__init__.py
@@ -1,10 +1,8 @@
-from .sort_options import SortOptions
 from .bars import BarSwitcher
 from .trees.model_tree import ModelTree
 
 
 __all__ = [
-    "SortOptions",
     "BarSwitcher",
     "ModelTree",
 ]

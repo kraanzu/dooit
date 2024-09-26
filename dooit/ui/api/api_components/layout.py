206cc022671290263fba5088a3d8fbef8ca2e662
kraanzu <kraanzu@gmail.com>
2024-09-26 12:54:22 +0530

    add layout api base

--- dooit/ui/api/api_components/layout.py
+++ dooit/ui/api/api_components/layout.py
@@ -1,4 +1,24 @@
+from dooit.ui.api.widgets import TodoLayout, WorkspaceLayout
 from ._base import ApiComponent
 
+
 class LayoutManager(ApiComponent):
-    pass
+    def __init__(self) -> None:
+        self._todo_layout: TodoLayout = []
+        self._workspace_layout: WorkspaceLayout = []
+
+    @property
+    def todo_layout(self) -> TodoLayout:
+        return self._todo_layout
+
+    @todo_layout.setter
+    def todo_layout(self, layout: TodoLayout):
+        self._todo_layout = layout
+
+    @property
+    def workspace_layout(self) -> WorkspaceLayout:
+        return self._workspace_layout
+
+    @workspace_layout.setter
+    def workspace_layout(self, layout: WorkspaceLayout):
+        self._workspace_layout = layout

91a1d97e95932103dc4b1df61ac9a3088285f53c
kraanzu <kraanzu@gmail.com>
2024-09-27 15:01:01 +0530

    add trigger refresh hook for formatter store

--- dooit/ui/api/api_components/formatters/formatter_store.py
+++ dooit/ui/api/api_components/formatters/formatter_store.py
@@ -9,14 +9,26 @@ class FormatterFunc:
     func: Callable
 
 
-class FormatterStore:
-    def __init__(self) -> None:
-        self.formatters = dict()
+def trigger_refresh(func: Callable) -> Callable:
+    def wrapper(self: "FormatterStore", *args, **kwargs):
+        res = func(self, *args, **kwargs)
+        self.trigger()
+        return res
 
+    return wrapper
+
+
+class FormatterStore:
+    def __init__(self, trigger: Callable) -> None:
+        self.formatters = dict()
+        self.trigger = trigger
+
+    @trigger_refresh
     def add(self, func: Callable, id: Optional[str] = None) -> str:
         id = id or uuid4().hex
         self.formatters[id] = FormatterFunc(id, func)
         return id
 
+    @trigger_refresh
     def remove(self, id: str) -> None:
         self.formatters.pop(id, None)

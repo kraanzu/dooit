ec6c46670d7ae6677f2974ac7ad95b0288c0cac0
kraanzu <kraanzu@gmail.com>
2024-09-27 12:01:42 +0530

    impl formatter store

--- dooit/ui/api/api_components/formatters/formatter_store.py
+++ dooit/ui/api/api_components/formatters/formatter_store.py
@@ -1,3 +1,22 @@
+from typing import Callable, Optional
+from uuid import uuid4
+from dataclasses import dataclass
+
+
+@dataclass
+class FormatterFunc:
+    name: str
+    func: Callable
+
+
 class FormatterStore:
     def __init__(self) -> None:
         self.formatters = dict()
+
+    def add(self, func: Callable, id: Optional[str] = None) -> str:
+        id = id or uuid4().hex
+        self.formatters[id] = FormatterFunc(id, func)
+        return id
+
+    def remove(self, id: str) -> None:
+        self.formatters.pop(id, None)

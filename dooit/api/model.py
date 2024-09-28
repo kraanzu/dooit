30bbfb3cd9e40af207435672bdfe10976eeb10e3
kraanzu <kraanzu@gmail.com>
2024-09-28 23:25:22 +0530

    clean: remove old order index impl

--- dooit/api/model.py
+++ dooit/api/model.py
@@ -155,26 +155,6 @@ class DooitModel(BaseModel, BaseModelMixin):
 
         return True
 
-    def set_order_index(self, index: int) -> None:
-        if index > len(self.siblings) or index < 0:
-            index = len(self.siblings)
-
-        cls = self.__class__
-        manager.session.query(cls).filter(cls.order_index >= index).update(
-            {cls.order_index: cls.order_index + 1},
-            synchronize_session=False,
-        )
-
-        siblings = [
-            i for i in self.siblings if i.id != self.id and i.order_index >= index
-        ]
-        for i in siblings[::-1]:
-            i.order_index += 1
-            i.save()
-
-        self.order_index = index
-        self.save()
-
     def drop(self) -> None:
         manager.delete(self)
 

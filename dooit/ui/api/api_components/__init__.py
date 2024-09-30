3d423fcd1ed2646c2fec5df0a5e4f88e9f162306
kraanzu <kraanzu@gmail.com>
2024-09-30 10:45:21 +0530

    ruff: format

--- dooit/ui/api/api_components/__init__.py
+++ dooit/ui/api/api_components/__init__.py
@@ -10,5 +10,5 @@ __all__ = [
     "LayoutManager",
     "VarManager",
     "Formatter",
-    "DashboardManager"
+    "DashboardManager",
 ]
--- dooit/ui/api/api_components/dashboard.py
+++ dooit/ui/api/api_components/dashboard.py
@@ -3,6 +3,5 @@ from ._base import ApiComponent
 
 
 class DashboardManager(ApiComponent):
-
     def set(self, labels: List):
         pass

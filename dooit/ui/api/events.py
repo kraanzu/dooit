dfeb114718330cbc916e61b1c5443de9f8d90695
kraanzu <kraanzu@gmail.com>
2024-10-04 18:35:51 +0530

    feat: add more events

--- dooit/ui/api/events.py
+++ dooit/ui/api/events.py
@@ -14,3 +14,14 @@ class Hook:
 startup = Hook(Startup)
 shutdown = Hook(ShutDown)
 mode_changed = Hook(ModeChanged)
+
+workspace_selected = Hook(WorkspaceSelected)
+workspace_description_changed = Hook(WorkspaceDescriptionChanged)
+
+todo_selected = Hook(TodoSelected)
+todo_description_changed = Hook(TodoDescriptionChanged)
+todo_due_changed = Hook(TodoDueChanged)
+todo_status_changed = Hook(TodoStatusChanged)
+todo_urgency_changed = Hook(TodoUrgencyChanged)
+todo_effort_changed = Hook(TodoEffortChanged)
+todo_recurrence_changed = Hook(TodoRecurrenceChanged)

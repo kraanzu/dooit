b6ec275a413e25725d5c23a3666179bb32e53edb
kraanzu <kraanzu@gmail.com>
2024-10-05 13:12:26 +0530

    update flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,15 @@
 {
   description = "Flake for Dooit";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-  }:
+  }
+  :
     flake-utils.lib.eachDefaultSystem (
       system: let
         name = "dooit";
@@ -47,7 +48,7 @@
           buildInputs = mainPkgs;
 
           # TODO: enable this
-          doCheck = false; 
+          doCheck = false;
         };
 
         # Deps: Devshell

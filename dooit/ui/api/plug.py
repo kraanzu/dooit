1df11c32acf17aaee5a0e646bec817c7dfb7d20b
kraanzu <kraanzu@gmail.com>
2024-10-12 22:02:08 +0530

    register @timer decorated functions

--- dooit/ui/api/plug.py
+++ dooit/ui/api/plug.py
@@ -3,8 +3,9 @@ from collections import defaultdict
 from pathlib import Path
 from typing import Callable, List
 from platformdirs import user_config_dir
+from textual.app import App
 
-from dooit.ui.api.events import DOOIT_EVENT_ATTR
+from dooit.ui.api.events import DOOIT_EVENT_ATTR, DOOIT_TIMER_ATTR
 from dooit.ui.events.events import DooitEvent
 from .loader import load_dir, load_file
 
@@ -20,8 +21,9 @@ DEFAULT_CONFIG = BASE_PATH / "utils" / "default_config.py"
 
 
 class PluginManager:
-    def __init__(self) -> None:
+    def __init__(self, app: App) -> None:
         self.events = defaultdict(list)
+        self.app = app
 
     def scan(self):
         load_file(self, DEFAULT_CONFIG)
@@ -31,6 +33,13 @@ class PluginManager:
         for event in events:
             self.events[event].append(obj)
 
+    def _register_timer(self, obj):
+        if interval := getattr(obj, DOOIT_TIMER_ATTR, None):
+            self.app.set_interval(interval, obj)
+
     def register(self, obj):
         if event := getattr(obj, DOOIT_EVENT_ATTR, None):
             self._register_events(event, obj)
+
+        if getattr(obj, DOOIT_TIMER_ATTR, None):
+            self._register_timer(obj)

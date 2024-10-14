090b7b8c627962c64997bff96e18d5c525ce465e
kraanzu <kraanzu@gmail.com>
2024-10-14 10:55:26 +0530

    add more default themes

---
+++ dooit/api/themes/__init__.py
@@ -0,0 +1,7 @@
+from .nord import Nord
+from .dracula import Dracula
+from .gruvbox import Gruvbox
+from .nymph import Nymph
+from .material import Material
+
+__all__ = ["Nord", "Dracula", "Gruvbox", "Nymph", "Material"]
---
+++ dooit/api/themes/gruvbox.py
@@ -0,0 +1,26 @@
+from ..theme import DooitThemeBase
+
+
+class Gruvbox(DooitThemeBase):
+    background_1: str = "#282828"  # Darkest
+    background_2: str = "#3c3836"  # Lighter
+    background_3: str = "#504945"  # Lightest
+
+    # foreground colors
+    foreground_1: str = "#7c6f64"  # Darkest
+    foreground_2: str = "#ebdbb2"  # Lighter
+    foreground_3: str = "#fbf1c7"  # Lightest
+
+    # other colors
+    red: str = "#fb4934"
+    orange: str = "#fe8019"
+    yellow: str = "#fabd2f"
+    green: str = "#b8bb26"
+    blue: str = "#83a598"
+    purple: str = "#d3869b"
+    magenta: str = "#d3869b"
+    cyan: str = "#8ec07c"
+
+    # accent colors
+    primary: str = yellow
+    secondary: str = green

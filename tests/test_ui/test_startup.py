401b788e55e2422fa64065762c81c78037eb03da
kraanzu <kraanzu@gmail.com>
2024-10-15 10:55:00 +0530

    test: update startup check

--- tests/test_ui/test_startup.py
+++ tests/test_ui/test_startup.py
@@ -1,6 +1,16 @@
 from tests.test_ui.ui_base import run_pilot
+from dooit.ui.tui import Dooit
 
 
 async def test_startup():
     async with run_pilot() as pilot:
-        assert pilot.app.is_running
+        app = pilot.app
+        assert isinstance(app, Dooit)
+
+        assert app.is_running
+
+        # check for vars
+        app.bar_switcher
+        app.workspace_tree
+
+        assert app.get_mode() == "NORMAL"

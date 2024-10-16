41d79c96ed8af4239d79c456ab0c8c00003a4eb2
kraanzu <kraanzu@gmail.com>
2024-10-16 21:37:23 +0530

    test: test todo tree switch

--- tests/test_ui/test_workspace_tree.py
+++ tests/test_ui/test_workspace_tree.py
@@ -1,3 +1,4 @@
+from asyncio import sleep
 from textual.widgets import ContentSwitcher
 from dooit.ui.widgets.trees.todos_tree import TodosTree
 from tests.test_ui.ui_base import run_pilot
@@ -24,9 +25,12 @@ async def test_workspaces_tree():
         # highlights
         wtree.highlight_id(w)
         assert wtree.highlighted == 3  # n-1
-        # current = app.query_one("#todo_switcher", expect_type=ContentSwitcher).visible_content
-        # assert current is not None
-        # assert current.id == TodosTree(wtree.current_model).id
+
+        await sleep(0.5)
+
+        current = app.query_one("#todo_switcher", expect_type=ContentSwitcher).visible_content
+        assert current is not None
+        assert current.id == TodosTree(wtree.current_model).id
 
         # child nodes
         w = wtree.add_child_node()
@@ -44,3 +48,17 @@ async def test_workspaces_tree():
         assert len(wtree._options) == 5
 
         # switch
+
+
+# async def test_base_addition():
+#     async with run_pilot() as pilot:
+#         app = pilot.app
+#         assert isinstance(app, Dooit)
+#
+#         wtree = app.workspace_tree
+#
+#         wtree.add_sibling()
+#         assert wtree.highlighted == 0
+#
+#         wtree.add_sibling()
+#         # assert wtree.highlighted == 1

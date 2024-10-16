c45b4aa2fb69b03ca187ef10f8da3e9c20b8d65d
kraanzu <kraanzu@gmail.com>
2024-10-16 22:25:36 +0530

    tests: add basic todo tree creation helper

--- tests/test_ui/ui_base.py
+++ tests/test_ui/ui_base.py
@@ -1,6 +1,23 @@
+from textual.pilot import Pilot
 from dooit.ui.tui import Dooit
 
 TEMP_CONN = "sqlite:///:memory:"
 
+
 def run_pilot():
     return Dooit(connection_string=TEMP_CONN).run_test()
+
+
+async def create_and_move_to_todo(pilot: Pilot):
+    app = pilot.app
+    assert isinstance(app, Dooit)
+
+    wtree = app.workspace_tree
+    wtree.add_sibling()
+    await pilot.pause()
+
+    await pilot.press("escape")
+    await pilot.pause()
+
+    app.api.switch_focus()
+    await pilot.pause()

7fe5e83bf12eafd4f15fd44981f4872936833352
kraanzu <kraanzu@gmail.com>
2024-10-16 17:34:12 +0530

    add more tests for workspaces tree

--- tests/test_ui/test_workspace_tree.py
+++ tests/test_ui/test_workspace_tree.py
@@ -2,7 +2,7 @@ from tests.test_ui.ui_base import run_pilot
 from dooit.ui.tui import Dooit
 
 
-async def workspaces_tree():
+async def test_workspaces_tree():
     async with run_pilot() as pilot:
         app = pilot.app
         assert isinstance(app, Dooit)
@@ -19,5 +19,17 @@ async def workspaces_tree():
         assert len(wtree._options) == 4
 
         wtree.highlight_id(w)
-
         assert wtree.highlighted == 3  # n-1
+
+        w = wtree.add_child_node()
+        assert len(wtree._options) == 5
+        assert wtree.highlighted == 4
+
+        wtree.toggle_expand()
+        assert len(wtree._options) == 5
+
+        wtree.toggle_expand_parent()
+        assert len(wtree._options) == 4
+
+        wtree.toggle_expand()
+        assert len(wtree._options) == 5

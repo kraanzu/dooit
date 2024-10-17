d747d034a71a809c0faa0a97504e2123d57df955
kraanzu <kraanzu@gmail.com>
2024-10-17 22:51:17 +0530

    tests: help screen with keybind

--- tests/test_ui/test_help_screen.py
+++ tests/test_ui/test_help_screen.py
@@ -1,5 +1,6 @@
 from tests.test_ui.ui_base import run_pilot
 from dooit.ui.tui import Dooit
+from dooit.ui.screens import HelpScreen
 
 
 async def test_help_screen_mount():
@@ -7,4 +8,12 @@ async def test_help_screen_mount():
         app = pilot.app
         assert isinstance(app, Dooit)
 
+        # test if help screen is mounted
         await app.push_screen("help")
+        assert app.screen.__class__ == HelpScreen
+
+        await app.push_screen("main")
+
+        # test with keybinding
+        await pilot.press("?")
+        assert app.screen.__class__ == HelpScreen

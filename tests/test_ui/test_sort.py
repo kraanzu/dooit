aa891c1a1d49d474c5da2b730f1494da3dd8e1c4
kraanzu <kraanzu@gmail.com>
2024-10-18 22:00:10 +0530

    test: sort bar
    TODO: fix tests

--- tests/test_ui/test_sort.py
+++ tests/test_ui/test_sort.py
@@ -81,7 +81,7 @@ async def test_options_highlight():
         assert sort_bar.selected == 1
 
 
-async def test_description_sort():
+async def test_reverse_sort():
     async with run_pilot() as pilot:
         app = pilot.app
         assert isinstance(app, Dooit)
@@ -111,9 +111,84 @@ async def test_description_sort():
         current_bar = app.bar_switcher.visible_content
         assert isinstance(current_bar, StatusBar)
 
+        # FIXME: This is not working for some reason
+        #
         # new_options = [node.prompt for node in tree._options]
         # assert current_options != new_options
+        # await sleep(0.2)
+        # await pilot.pause()
+        # assert tree.highlighted == 0 # sorted in reverse
 
+
+async def test_sort_cancelled():
+    async with run_pilot() as pilot:
+        app = pilot.app
+        assert isinstance(app, Dooit)
+        api = app.api
+
+        tree = await create_and_move_to_todo(pilot)
+
+        tree.add_sibling()
+        await pilot.press(*list("zzz"))
+        await pilot.press("escape")
+
+        tree.add_sibling()
+        await pilot.press(*list("abcd"))
+        await pilot.press("escape")
+
+        current_options = [node.prompt for node in tree._options]
+
+        api.start_sort()
+        await pilot.pause()
+
+        sort_bar = app.bar_switcher.visible_content
+        assert isinstance(sort_bar, SortBar)
+        assert tree.highlighted == 1  # sorted in reverse
+
+        await pilot.press("escape")
+        await pilot.pause()
+        current_bar = app.bar_switcher.visible_content
+        assert isinstance(current_bar, StatusBar)
+
+        new_options = [node.prompt for node in tree._options]
+        assert current_options == new_options
+
+
+async def test_description_sort():
+    async with run_pilot() as pilot:
+        app = pilot.app
+        assert isinstance(app, Dooit)
+        api = app.api
+
+        tree = await create_and_move_to_todo(pilot)
+
+        tree.add_sibling()
+        await pilot.press(*list("zzz"))
+        await pilot.press("escape")
+
+        tree.add_sibling()
+        await pilot.press(*list("abcd"))
+        await pilot.press("escape")
+
+        # current_options = [node.prompt for node in tree._options]
+
+        api.start_sort()
+        await pilot.pause()
+
+        sort_bar = app.bar_switcher.visible_content
+        assert isinstance(sort_bar, SortBar)
+        assert tree.highlighted == 1  # sorted in reverse
+
+        sort_bar.selected = 1
+        await pilot.press("enter")
+        await pilot.pause()
+        current_bar = app.bar_switcher.visible_content
+        assert isinstance(current_bar, StatusBar)
+
+        # FIXME: This is not working for some reason
+        #
+        # new_options = [node.prompt for node in tree._options]
+        # assert current_options != new_options
         # await sleep(0.2)
         # await pilot.pause()
         # assert tree.highlighted == 0 # sorted in reverse

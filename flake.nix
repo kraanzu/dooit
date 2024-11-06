630cb87e2b3afb1e0e651853b75876c64042ef87
kraanzu <kraanzu@gmail.com>
2024-11-06 22:46:46 +0530

    refactored flake.nix

--- flake.nix
+++ flake.nix
@@ -1,78 +1,99 @@
 {
   description = "Flake for Dooit";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }
-  :
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        name = "dooit";
+  outputs = inputs: let
+    # Function to generate attributes for each system
+    forEachSystem = inputs.nixpkgs.lib.genAttrs inputs.nixpkgs.lib.platforms.all;
+
+    # Import nixpkgs for each system
+    pkgsFor = forEachSystem (
+      system:
+        import inputs.nixpkgs {
+          inherit system;
+        }
+    );
+
+    # Common Python packages used in both main package and devShell
+    mainPkgs = python3: with python3; [
+      poetry-core
+      pyperclip
+      textual
+      pyyaml
+      dateutil
+      sqlalchemy
+      platformdirs
+      tzlocal
+      click
+    ];
+
+    # Additional packages for the devShell
+    devShellPkgs = python3: with python3; [
+      textual-dev
+      pre-commit-hooks
+      pytest
+      pytest-aio
+      faker
+    ];
+
+    # Native build inputs
+    nativeBuildInputs = pkgs: with pkgs; [
+      poetry
+    ];
+
+    # Define devShell for each system
+    devShellFor = system:
+      inputs.nixpkgs.lib.genAttrs ["default"] (
+        _:
+          pkgsFor.${system}.mkShell {
+            buildInputs = (mainPkgs pkgsFor.${system}.python312Packages)
+              ++ (devShellPkgs pkgsFor.${system}.python312Packages)
+              ++ [ pkgsFor.${system}.bun ];
+
+            shellHook = ''
+              cd site/
+              ${pkgsFor.${system}.bun}/bin/bun install
+              cd ..
+            '';
+          }
+      );
+
+    # Define default package for each system
+    defaultPackageFor = system:
+      pkgsFor.${system}.python312Packages.buildPythonPackage {
+        pname = "dooit";
         version = "3.0.0";
+        src = ./.;
+        format = "pyproject";
 
-        pkgs = import nixpkgs {inherit system;};
-        python3 = pkgs.python312Packages;
+        nativeBuildInputs = nativeBuildInputs pkgsFor.${system};
 
-        mainPkgs = with python3; [
-          poetry-core
-          pyperclip
-          textual
-          pyyaml
-          dateutil
-          sqlalchemy
-          platformdirs
-          tzlocal
-          click
+        pythonRelaxDeps = [
+          "textual"
+          "tzlocal"
+          "platformdirs"
+          "sqlalchemy"
         ];
-      in {
-        packages.default = python3.buildPythonPackage {
-          pname = name;
-          version = version;
-          src = ./.;
-          format = "pyproject";
 
-          nativeBuildInputs = with pkgs; [
-            poetry
-          ];
+        buildInputs = mainPkgs pkgsFor.${system}.python312Packages;
+        propagatedBuildInputs = mainPkgs pkgsFor.${system}.python312Packages;
 
-          pythonRelaxDeps = [
-            "textual"
-            "tzlocal"
-            "platformdirs"
-            "sqlalchemy"
-          ];
+        doCheck = false;
+      };
 
-          buildInputs = mainPkgs;
-          propagatedBuildInputs = mainPkgs;
+  in {
+    # Define the devShells for each system
+    devShells = forEachSystem devShellFor;
 
-          # TODO: enable this
-          doCheck = false;
-        };
-
-        # Deps: Devshell
-        devShell = pkgs.mkShell {
-          name = "dooit";
-          buildInputs =
-            mainPkgs
-            ++ (with python3; [
-              textual-dev
-              pre-commit-hooks
-              pytest
-              pytest-aio
-              faker
-            ])
-            ++ [pkgs.bun];
-          shellHook = ''
-            cd site/
-            ${pkgs.bun}/bin/bun install
-            cd ..
-          '';
-        };
+    # Define the default package for each system
+    packages = forEachSystem (
+      system: {
+        default = defaultPackageFor system;
       }
     );
+  };
 }
+

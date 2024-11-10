e26b43bcf7a8797c62bfc7b69915d18404de7a09
kraanzu <kraanzu@gmail.com>
2024-11-10 12:49:49 +0530

    fix: error when user_data_dir does not exist

--- dooit/api/_vars.py
+++ dooit/api/_vars.py
@@ -4,3 +4,5 @@ from platformdirs import user_data_dir
 ROOT_FOLDER = Path(user_data_dir("dooit"))
 DATABASE_FILE = ROOT_FOLDER / "dooit.db"
 DATABASE_CONN_STRING = f"sqlite:////{DATABASE_FILE}"
+
+DATABASE_FILE.parent.mkdir(parents=True, exist_ok=True)

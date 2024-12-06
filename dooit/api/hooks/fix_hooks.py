6fed56b11081d7d6ca26800c810fd591160a677d
kraanzu <kraanzu@gmail.com>
2024-12-06 08:11:23 +0530

    feat: auto adjust order_index

--- dooit/api/hooks/fix_hooks.py
+++ dooit/api/hooks/fix_hooks.py
@@ -3,7 +3,6 @@ from ..workspace import Workspace
 from ..todo import Todo
 
 
-@event.listens_for(Workspace, "before_update")
 @event.listens_for(Workspace, "before_insert")
 def fix_order_id_workspace(mapper, connection, target: Workspace):
     if target.is_root:
@@ -11,10 +10,32 @@ def fix_order_id_workspace(mapper, connection, target: Workspace):
 
     if target.order_index is None or target.order_index == -1:
         target.order_index = len(target.siblings) - 1
+        return
+
+    if target.order_index >= 0:
+        connection.execute(
+            """
+            UPDATE todos
+            SET order_index = order_index + 1
+            WHERE order_index >= :current_index
+            AND id != :target_id
+            """,
+            {"current_index": target.order_index, "target_id": target.id},
+        )
 
 
 @event.listens_for(Todo, "before_insert")
-@event.listens_for(Todo, "before_update")
 def fix_order_id_todo(mapper, connection, target: Todo):
     if target.order_index is None or target.order_index == -1:
         target.order_index = len(target.siblings) - 1
+
+    if target.order_index >= 0:
+        connection.execute(
+            """
+            UPDATE workspaces
+            SET order_index = order_index + 1
+            WHERE order_index >= :current_index
+            AND id != :target_id
+            """,
+            {"current_index": target.order_index, "target_id": target.id},
+        )
